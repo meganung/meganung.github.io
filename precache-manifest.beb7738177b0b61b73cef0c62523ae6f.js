8d31217ff5058be8af678f3271d151890c0ea60a
Megan Ung <ung.meg@gmail.com>
2019-01-10 01:41:59 -0500

    Updates

---
+++ precache-manifest.beb7738177b0b61b73cef0c62523ae6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "09937616cb1d98c84ea91c8cf653952e",
+    "url": "/static/media/skeeball.09937616.png"
+  },
+  {
+    "revision": "39f1a2d35835921bab76",
+    "url": "/static/css/main.f2ff06ed.chunk.css"
+  },
+  {
+    "revision": "62491cf653c4d2902204",
+    "url": "/static/js/1.62491cf6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "41135ab2b93fb3cbc0199bade1355408",
+    "url": "/static/media/prof1.41135ab2.jpeg"
+  },
+  {
+    "revision": "72ab382e6d1ab70daaa456f1a95a128a",
+    "url": "/static/media/prof2.72ab382e.jpeg"
+  },
+  {
+    "revision": "39f1a2d35835921bab76",
+    "url": "/static/js/main.39f1a2d3.chunk.js"
+  },
+  {
+    "revision": "8daa0e8a157eff6dae61b56bc255f78a",
+    "url": "/static/media/gpsapp.8daa0e8a.png"
+  },
+  {
+    "revision": "d0f15be82bcca5f363d6de7fd538b712",
+    "url": "/static/media/weatherapp.d0f15be8.png"
+  },
+  {
+    "revision": "26da38c999e5649a9bd571939bdf010a",
+    "url": "/static/media/skeeball.26da38c9.zip"
+  },
+  {
+    "revision": "1e9c8710b904f5fe81e9e7276d797ac8",
+    "url": "/static/media/spotus.1e9c8710.png"
+  },
+  {
+    "revision": "1673932bbab3a4b2fb51ea8c2e3e7f29",
+    "url": "/static/media/resq.1673932b.png"
+  },
+  {
+    "revision": "148d6d5adf046977c7f807bff3031116",
+    "url": "/index.html"
+  }
+];
